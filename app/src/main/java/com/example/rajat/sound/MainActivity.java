f130c4f55132c6554054b584b42351c4afd0c40f
therajat08 <therajat08@users.noreply.github.com>
2019-01-13 19:01:48 +0530

    At this point new seekbar is added which allows us to see different positions in audio timeline and also allows us to jump to a position in timeline

--- app/src/main/java/com/example/rajat/sound/MainActivity.java
+++ app/src/main/java/com/example/rajat/sound/MainActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mplayer;
@@ -63,5 +66,40 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //getting the seekbar for controlling playtime of mp3
+        final SeekBar scrubber =  (SeekBar) findViewById(R.id.scrubber);
+        scrubber.setMax(mplayer.getDuration());//this line is to get duration of audio file
+
+        //with below method we are trying to rum code within run() in  every 1000 period
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                //code here is run periodically
+                scrubber.setProgress(mplayer.getCurrentPosition());
+                //with this code frequently we get where the audio file is in its timeline
+            }
+        },0, 100);//0 means it is run immediately for the first time
+
+        scrubber.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                mplayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+
+
+
 }
